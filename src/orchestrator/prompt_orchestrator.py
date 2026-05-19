70d437fb50ff27c189226258d0353e77d8d5465e
nicosammito <nsammito@code0.tech>
2026-05-20 00:01:49 +0200

    refactor prompt_orchestrator.py to update return type and increase max retries and max tokens

--- src/orchestrator/prompt_orchestrator.py
+++ src/orchestrator/prompt_orchestrator.py
@@ -1,12 +1,12 @@
 import os
-from typing import List
+from typing import List, Any
 
 import instructor
-from litellm import completion, max_tokens
+from litellm import completion
 from litellm.types.completion import ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam
 
-from src.schema.flow_type_schema import FlowType
 from src.schema.flow_schema import Flow
+from src.schema.flow_type_schema import FlowType
 from src.schema.function_schema import FunctionDefinition
 
 llm_api_key = os.getenv("LLM_API_KEY")
@@ -25,7 +25,7 @@ class PromptOrchestrator:
             few_shots: List[ChatCompletionUserMessageParam],
             available_flow_types: List[FlowType],
             available_functions: List[FunctionDefinition]
-    ) -> Flow:
+    ) -> tuple[Flow, Any]:
         flow_types_json = [f"{t.identifier}{t.signature}" for t in available_flow_types]
         functions_json = [f"{f.identifier}{f.signature}" for f in available_functions]
 
@@ -48,21 +48,19 @@ class PromptOrchestrator:
             {"role": "assistant", "content": "{"}
         ]
 
-        print("messages", messages)
-
         flow, completion = self.client.chat.completions.create_with_completion(
             model=self.model,
             response_model=Flow,
             api_key=llm_api_key,
             api_base=llm_api_base,
             messages=messages,
-            max_retries=3,
+            max_retries=5,
             strict=True,
             timeout=20,
-            max_tokens=10000,
+            max_tokens=50000,
             top_p=0.1,
             temperature=0.0,
             include_reasoning=False,
         )
 
-        return flow
+        return flow, completion
