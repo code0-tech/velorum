98d7cb867068833decdf17ec9e207ebe3fb1983d
nicosammito <nsammito@code0.tech>
2026-05-21 01:09:50 +0200

    add flow_value_mapper for mapping between gRPC NodeValue and NodeParameterValue schemas

---
+++ src/mapper/flow_value_mapper.py
@@ -0,0 +1,60 @@
+import tucana.generated.shared.flow_pb2 as flow_pb2
+
+from src.mapper.flow_literal_value_mapper import map_to_grpc_literal_value, map_to_literal_value_schema
+from src.schema.flow_schema import NodeParameterValue, LiteralValue, ReferencePath, TriggerReferenceValue, \
+    SubFlowReferenceValue, NodeReferenceValue, SubFlowValue
+
+
+def map_to_value_schema(grpc_value: flow_pb2.NodeValue) -> NodeParameterValue:
+    if grpc_value.literal_value:
+        return map_to_literal_value_schema(grpc_value.literal_value)
+    if grpc_value.reference_value:
+        ref_paths = None
+        if grpc_value.reference_value.paths:
+            ref_paths = [ReferencePath(path=p.path) for p in grpc_value.reference_value.paths]
+
+        if grpc_value.reference_value.flow_input:
+            return TriggerReferenceValue(referencePath=ref_paths)
+        elif grpc_value.reference_value.input_type:
+            return SubFlowReferenceValue(
+                nodeFunctionId=grpc_value.reference_value.input_type.node_id,
+                inputIndex=grpc_value.reference_value.input_type.input_index,
+                parameterIndex=grpc_value.reference_value.input_type.parameter_index,
+                referencePath=ref_paths
+            )
+        elif grpc_value.reference_value.node_id:
+            return NodeReferenceValue(
+                nodeFunctionId=grpc_value.reference_value.node_id,
+                referencePath=ref_paths
+            )
+    if grpc_value.sub_flow and grpc_value.sub_flow.starting_node_id:
+        return SubFlowValue(
+            startingNodeId=grpc_value.sub_flow.starting_node_id
+        )
+
+    return LiteralValue(value=None)
+
+
+def map_to_grpc_value(value: NodeParameterValue) -> flow_pb2.NodeValue:
+    grpc_val = flow_pb2.NodeValue()
+    if hasattr(value, 'value'):
+        grpc_val.literal_value = map_to_grpc_literal_value(value)
+    elif hasattr(value, 'starting_node_id'):
+        grpc_val.sub_flow.starting_node_id = value.starting_node_id
+    elif hasattr(value, 'node_id') and hasattr(value, 'input_index') and hasattr(value, 'parameter_index'):
+        grpc_val.reference_value.input_type.node_id = value.node_id
+        grpc_val.reference_value.input_type.parameter_index = value.parameter_index
+        grpc_val.reference_value.input_type.input_index = value.node_id
+    elif hasattr(value, 'node_id'):
+        grpc_val.reference_value.node_id = value.node_id
+    elif hasattr(value, 'reference_path'):
+        grpc_val.reference_value.flow_input = flow_pb2.FlowInput()
+
+    if value.reference_path:
+        for p in value.reference_path:
+            grpc_path = flow_pb2.ReferencePath()
+            if p.path:
+                grpc_path.path = p.path
+            grpc_val.reference_value.paths.append(grpc_path)
+
+    return grpc_val
