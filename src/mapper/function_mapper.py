d8f5d5cdfc0f90d87f860a03dbcfb66b180aff63
nicosammito <nsammito@code0.tech>
2026-05-20 17:35:54 +0200

    add function_mapper.py for mapping FunctionDefinition to gRPC format

--- src/mapper/function_mapper.py
+++ src/mapper/function_mapper.py
@@ -12,9 +12,49 @@ def map_to_function_schema(grpc_value: function_pb2.FunctionDefinition) -> Funct
         descriptions=grpc_value.description[0].content if grpc_value.description else "",
         parameterDefinitions=[
             ParameterDefinition(
-                defaultValue=parameter.default_value,
                 names=parameter.name[0].content if parameter.name else "",
                 descriptions=parameter.description[0].content if parameter.description else "",
             ) for parameter in grpc_value.parameter_definitions
+        ]
+    )
+
+def map_to_grpc_function(function: FunctionDefinition) -> function_pb2.FunctionDefinition:
+    return function_pb2.FunctionDefinition(
+        runtime_name=function.identifier,
+        signature=function.signature,
+        name=[
+            {
+                "code": "en-US",
+                "content": function.names if function.names else ""
+            }
+        ],
+        alias=[
+            {
+                "code": "en-US",
+                "content": function.aliases if function.aliases else ""
+            }
+        ],
+        description=[
+            {
+                "code": "en-US",
+                "content": function.descriptions if function.descriptions else ""
+            }
+        ],
+        parameter_definitions=[
+            {
+                "default_value": parameter.default_value,
+                "name": [
+                    {
+                        "code": "en-US",
+                        "content": parameter.names if parameter.names else ""
+                    }
+                ],
+                "description": [
+                    {
+                        "code": "en-US",
+                        "content": parameter.descriptions if parameter.descriptions else ""
+                    }
+                ]
+            } for parameter in (function.parameter_definitions or [])
         ]
     )
