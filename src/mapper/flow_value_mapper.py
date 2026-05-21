5ba7e46567608f7655f7d450b33cbb6c5310debf
nicosammito <nsammito@code0.tech>
2026-05-21 15:17:00 +0200

    refactor flow_value_mapper to use HasField for improved value schema mapping

--- src/mapper/flow_value_mapper.py
+++ src/mapper/flow_value_mapper.py
@@ -6,28 +6,28 @@ from src.schema.flow_schema import NodeParameterValue, LiteralValue, ReferencePa
 
 
 def map_to_value_schema(grpc_value: flow_pb2.NodeValue) -> NodeParameterValue:
-    if grpc_value.literal_value:
+    if grpc_value.HasField('literal_value'):
         return map_to_literal_value_schema(grpc_value.literal_value)
-    if grpc_value.reference_value:
+    elif grpc_value.HasField('reference_value'):
         ref_paths = None
-        if grpc_value.reference_value.paths:
+        if len(grpc_value.reference_value.paths) > 0:
             ref_paths = [ReferencePath(path=p.path) for p in grpc_value.reference_value.paths]
 
-        if grpc_value.reference_value.flow_input:
+        if grpc_value.reference_value.HasField("flow_input"):
             return TriggerReferenceValue(referencePath=ref_paths)
-        elif grpc_value.reference_value.input_type:
+        elif grpc_value.reference_value.HasField("input_type"):
             return SubFlowReferenceValue(
                 nodeFunctionId=grpc_value.reference_value.input_type.node_id,
                 inputIndex=grpc_value.reference_value.input_type.input_index,
                 parameterIndex=grpc_value.reference_value.input_type.parameter_index,
                 referencePath=ref_paths
             )
-        elif grpc_value.reference_value.node_id:
+        elif grpc_value.reference_value.HasField("node_id"):
             return NodeReferenceValue(
                 nodeFunctionId=grpc_value.reference_value.node_id,
                 referencePath=ref_paths
             )
-    if grpc_value.sub_flow and grpc_value.sub_flow.starting_node_id:
+    elif grpc_value.HasField('sub_flow') and grpc_value.sub_flow.HasField("starting_node_id"):
         return SubFlowValue(
             startingNodeId=grpc_value.sub_flow.starting_node_id
         )
