13dfc329e591cd749d6e756be09e709ff9adaa1d
nicosammito <nsammito@code0.tech>
2026-05-21 17:00:14 +0200

    feat: implement InfoService for gRPC with model retrieval

---
+++ src/endpoint/info/info_endpoint.py
@@ -0,0 +1,13 @@
+import tucana.generated.velorum.info_pb2 as pb2
+import tucana.generated.velorum.info_pb2_grpc as pb2_grpc
+
+from src.mapper.model_mapper import map_to_grpc_model
+from src.store.model_store import ModelStore
+
+
+class InfoService(pb2_grpc.InfoServiceServicer):
+    def __init__(self):
+        self.model_store = ModelStore()
+
+    def Models(self, request: pb2.ModelsRequest, context) -> pb2.ModelsResponse:
+        return pb2.ModelsResponse(models=[map_to_grpc_model(model) for model in self.model_store.get_all()])
