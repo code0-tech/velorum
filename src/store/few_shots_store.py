b1080b922450c96689ffa7c5dbec0ac5cc423a78
nicosammito <nsammito@code0.tech>
2026-06-11 14:04:20 +0200

    feat: few shots store and schema

---
+++ src/store/few_shots_store.py
@@ -0,0 +1,62 @@
+import json
+from pathlib import Path
+from typing import Any, List
+
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
+
+from src.schema.few_shot_schema import FewShot
+from src.store.vector_store import Store
+
+
+class FewShotsStore(Store):
+    def __init__(self, memory_client: QdrantClient, vector_model: SentenceTransformer):
+        super().__init__(
+            memory_client,
+            vector_model,
+            'few_shots',
+            "identifier",
+            "identifier"
+        )
+
+    def _load_models_from_json(self):
+        filepath = Path(__file__).parent.parent.parent / "few_shots.configuration.json"
+
+        if not filepath.exists():
+            raise FileNotFoundError(f"File not found: {filepath}")
+
+        with open(filepath, 'r', encoding='utf-8') as f:
+            raw_data = json.load(f)
+
+        if not isinstance(raw_data, list):
+            raw_data = [raw_data]
+
+        for item in raw_data:
+            self.insert("global", FewShot(
+                **item
+            ))
+
+    def insert(self, group_identifier: str, payload: FewShot) -> None:
+        super().insert(group_identifier, payload)
+
+    def validate(self, payload: Any) -> FewShot:
+        return FewShot.model_validate(payload)
+
+    def search(self, group_identifier: str, prompt: str, limit=5) -> List[FewShot]:
+        return super().search(prompt, group_identifier, limit)
+
+    def find(self, group_identifier: str, identifier: str) -> FewShot | None:
+        return super().find(group_identifier, identifier)
+
+    def find_all(self, group_identifier: str, identifiers: List[str]) -> List[FewShot]:
+        return super().find_all(group_identifier, identifiers)
+
+    def get_all(self, group_identifier: str) -> List[FewShot]:
+        return super().get_all(group_identifier)
+
+    def combine(
+            self,
+            first_list: List[FewShot],
+            second_list: List[FewShot]
+    ) -> List[FewShot]:
+        return super().combine(first_list, second_list)
